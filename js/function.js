8d9281da1fc41a00b91213df5a7c58b3ebe6bdeb
pala20 <francopalazzolo.2791@gmail.com>
2023-02-28 13:44:13 -0300

    primer commit

---
+++ js/function.js
@@ -0,0 +1,23 @@
+function verMas(){
+    console.log("estoy dentro de la funcion")
+    Swal.fire(
+        'Perfil de LinkedIn',
+        'Franco Javier Palazzolo',
+       
+      )
+}
+function contacto1() {
+    Swal.fire(
+      '2604889545',
+    )
+}
+function contacto2() {
+    Swal.fire(
+      '2604154522',
+    )
+}
+function contacto3() {
+    Swal.fire(
+      '2604774111',
+    )
+}
